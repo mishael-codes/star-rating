ae9bee2e8775daf358a12471eb82b874d6b95c01
mishael-codes <enyimishael22@gmail.com>
2023-09-05 20:56:18 +0100

    initial commit

---
+++ src/StarRating.jsx
@@ -0,0 +1,22 @@
+function StarRating({ rating }) {
+
+  const ratingArray = Array.from({ length: rating });
+
+  return (
+    <>
+      <div className="star-wrapper">
+        {ratingArray.map((index) => (
+          <img 
+            key={index}
+            alt="gold-star"
+            className="gold-star"
+            src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+          />
+        ))}
+      </div>
+    </>
+  );
+
+}
+
+export default StarRating;
